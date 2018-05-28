d4af0719f3deecc368526c01c58228ab81ec76e4
BataLaSalade <dsfmarina@gmail.com>
2018-05-28 17:23:49 +0200

    Nouvelle refont du MPD et BDD / Ajout de la table produitsDuJour pour gerer les dates des plats du jour et prevoir une programmation a lavance des plats du jours avec une cardinalite 1:n et un champ date qui peut etre dupliquer pour autant de produit que necessaire / modification de la relation entre les tables Commande, detailCommande et Produit puisque jai ajouter la table produitsDuJours au milieu

--- expressfood.sql
+++ expressfood.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3307
--- Généré le :  lun. 28 mai 2018 à 13:14
+-- Généré le :  lun. 28 mai 2018 à 15:19
 -- Version du serveur :  10.2.8-MariaDB
 -- Version de PHP :  7.1.9
 
@@ -88,32 +88,33 @@ INSERT INTO `commande` (`id`, `Utilisateur_id`, `date`, `statutPaiement`, `statu
 DROP TABLE IF EXISTS `détail_commande`;
 CREATE TABLE IF NOT EXISTS `détail_commande` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `Commande_id` int(11) NOT NULL,
-  `Produit_id` int(11) NOT NULL,
   `quantité` int(11) NOT NULL DEFAULT 1,
-  PRIMARY KEY (`id`,`Commande_id`,`Produit_id`),
-  KEY `fk_Commande_has_Produit_Produit1_idx` (`Produit_id`),
-  KEY `fk_Commande_has_Produit_Commande_idx` (`Commande_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8;
+  `produitsDuJour_id` int(11) NOT NULL,
+  `Commande_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`,`produitsDuJour_id`,`Commande_id`),
+  KEY `fk_Détail_Commande_Commande1_idx` (`Commande_id`),
+  KEY `fk_Détail_Commande_produitsDuJour1_idx` (`produitsDuJour_id`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `détail_commande`
 --
 
-INSERT INTO `détail_commande` (`id`, `Commande_id`, `Produit_id`, `quantité`) VALUES
+INSERT INTO `détail_commande` (`id`, `quantité`, `produitsDuJour_id`, `Commande_id`) VALUES
 (1, 1, 1, 1),
 (2, 1, 3, 1),
-(3, 2, 2, 1),
-(4, 2, 4, 1),
-(5, 3, 1, 2),
-(6, 3, 3, 1),
-(7, 3, 4, 1),
-(8, 4, 2, 1),
-(9, 4, 4, 1),
-(10, 5, 1, 1),
-(11, 5, 3, 1),
-(12, 6, 2, 1),
-(13, 6, 4, 1);
+(3, 2, 3, 2),
+(4, 2, 2, 2),
+(5, 1, 5, 3),
+(6, 1, 7, 3),
+(7, 1, 6, 4),
+(8, 1, 7, 4),
+(9, 2, 8, 4),
+(10, 2, 5, 4),
+(11, 2, 6, 5),
+(12, 2, 8, 5),
+(13, 5, 1, 6),
+(14, 5, 7, 6);
 
 -- --------------------------------------------------------
 
@@ -127,12 +128,9 @@ CREATE TABLE IF NOT EXISTS `produit` (
   `Categorie_id` int(11) NOT NULL,
   `dateCreation` date NOT NULL,
   `dateModif` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  `onLine` tinyint(4) NOT NULL,
   `nom` varchar(70) NOT NULL,
   `description` longtext NOT NULL,
   `prixUnitaireHT` decimal(3,2) NOT NULL,
-  `startingDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `endingDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   PRIMARY KEY (`id`,`Categorie_id`),
   KEY `fk_Produit_Categorie1_idx` (`Categorie_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
@@ -141,19 +139,56 @@ CREATE TABLE IF NOT EXISTS `produit` (
 -- Déchargement des données de la table `produit`
 --
 
-INSERT INTO `produit` (`id`, `Categorie_id`, `dateCreation`, `dateModif`, `onLine`, `nom`, `description`, `prixUnitaireHT`, `startingDate`, `endingDate`) VALUES
-(1, 2, '2018-05-24', '2018-05-28 10:11:04', 1, 'Salade de fruits frais', 'Salade avec fruit frais de saison : pomme, poire, fraise et pamplemousse', '3.10', '2018-05-27 22:00:00', '2018-05-28 21:59:59'),
-(2, 2, '2018-05-24', '2018-05-28 10:12:20', 1, 'Dessert marbré', 'Dessert marbré, citron et framboise', '3.10', '2018-05-27 22:00:00', '2018-05-28 21:59:59'),
-(3, 1, '2018-05-24', '2018-05-28 10:12:20', 1, 'Salade du soleil', 'Salade de pâtes composées de pâtes, poulet, courguettes, tomates cerises et confites basilic', '6.70', '2018-05-27 22:00:00', '2018-05-28 21:59:59'),
-(4, 1, '2018-05-24', '2018-05-28 10:12:20', 1, 'Salade poulet verde', 'Salade de poulet dont tous les accompagnements sont vert : fèves, petit-pois, avocat, crème de roquette', '6.70', '2018-05-27 22:00:00', '2018-05-28 21:59:59'),
-(5, 2, '2018-05-25', '2018-05-28 10:15:07', 0, 'Fromage blanc', 'Fromage blanc du producteur du coin avec coulis au choix : crème de marron ou fruit rouge', '3.10', '2018-05-28 22:00:00', '2018-05-29 21:59:59'),
-(6, 2, '2018-05-25', '2018-05-28 10:15:07', 0, 'Verrine grecque', 'Verrine aux abricots, muesli, yahourt grec', '3.10', '2018-05-28 22:00:00', '2018-05-29 21:59:59'),
-(7, 1, '2018-05-25', '2018-05-28 10:15:07', 0, 'Salade Thaï', 'Salade thaï avec crevettes, carottes, courgette, nouilles, coriande et sésame', '6.70', '2018-05-28 22:00:00', '2018-05-29 21:59:59'),
-(8, 1, '2018-05-25', '2018-05-28 10:15:07', 0, 'Wrap mexicain', 'Wrap, poulet, guacamole, salade iceberg, oignon rouge et tomate', '6.70', '2018-05-28 22:00:00', '2018-05-29 21:59:59'),
-(9, 2, '2018-05-26', '2018-05-28 10:15:07', 0, 'Tarte coco', 'Tarte à la noix de coco  avec son coulis de chocolat intense', '3.10', '2018-05-29 22:00:00', '2018-05-30 21:59:59'),
-(10, 2, '2018-05-26', '2018-05-28 10:15:07', 0, 'Tarte chocolat', 'Délicieuse tarte au chocolat, surmontée d\'un crumble croquant', '3.10', '2018-05-29 22:00:00', '2018-05-30 21:59:59'),
-(11, 1, '2018-05-26', '2018-05-28 10:15:07', 0, 'Sandwich méditerranéen', 'Jambon cru, tapenade, tomates, fêta dans pain de tradition', '6.70', '2018-05-29 22:00:00', '2018-05-30 21:59:59'),
-(12, 1, '2018-05-26', '2018-05-28 10:15:07', 0, 'La Pesto', 'Salade de ravioli au fromage, sauce pesto, tomate séchée, pignons et parmesan', '6.70', '2018-05-29 22:00:00', '2018-05-30 21:59:59');
+INSERT INTO `produit` (`id`, `Categorie_id`, `dateCreation`, `dateModif`, `nom`, `description`, `prixUnitaireHT`) VALUES
+(1, 2, '2018-05-24', '2018-05-28 10:11:04', 'Salade de fruits frais', 'Salade avec fruit frais de saison : pomme, poire, fraise et pamplemousse', '3.10'),
+(2, 2, '2018-05-24', '2018-05-28 10:12:20', 'Dessert marbré', 'Dessert marbré, citron et framboise', '3.10'),
+(3, 1, '2018-05-24', '2018-05-28 10:12:20', 'Salade du soleil', 'Salade de pâtes composées de pâtes, poulet, courguettes, tomates cerises et confites basilic', '6.70'),
+(4, 1, '2018-05-24', '2018-05-28 10:12:20', 'Salade poulet verde', 'Salade de poulet dont tous les accompagnements sont vert : fèves, petit-pois, avocat, crème de roquette', '6.70'),
+(5, 2, '2018-05-25', '2018-05-28 10:15:07', 'Fromage blanc', 'Fromage blanc du producteur du coin avec coulis au choix : crème de marron ou fruit rouge', '3.10'),
+(6, 2, '2018-05-25', '2018-05-28 10:15:07', 'Verrine grecque', 'Verrine aux abricots, muesli, yahourt grec', '3.10'),
+(7, 1, '2018-05-25', '2018-05-28 10:15:07', 'Salade Thaï', 'Salade thaï avec crevettes, carottes, courgette, nouilles, coriande et sésame', '6.70'),
+(8, 1, '2018-05-25', '2018-05-28 10:15:07', 'Wrap mexicain', 'Wrap, poulet, guacamole, salade iceberg, oignon rouge et tomate', '6.70'),
+(9, 2, '2018-05-26', '2018-05-28 10:15:07', 'Tarte coco', 'Tarte à la noix de coco  avec son coulis de chocolat intense', '3.10'),
+(10, 2, '2018-05-26', '2018-05-28 10:15:07', 'Tarte chocolat', 'Délicieuse tarte au chocolat, surmontée d\'un crumble croquant', '3.10'),
+(11, 1, '2018-05-26', '2018-05-28 10:15:07', 'Sandwich méditerranéen', 'Jambon cru, tapenade, tomates, fêta dans pain de tradition', '6.70'),
+(12, 1, '2018-05-26', '2018-05-28 10:15:07', 'La Pesto', 'Salade de ravioli au fromage, sauce pesto, tomate séchée, pignons et parmesan', '6.70');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `produitsdujour`
+--
+
+DROP TABLE IF EXISTS `produitsdujour`;
+CREATE TABLE IF NOT EXISTS `produitsdujour` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `Produit_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`,`Produit_id`),
+  KEY `fk_platsDuJour_Produit1_idx` (`Produit_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `produitsdujour`
+--
+
+INSERT INTO `produitsdujour` (`id`, `date`, `Produit_id`) VALUES
+(1, '2018-05-27 22:00:00', 1),
+(2, '2018-05-27 22:00:00', 2),
+(3, '2018-05-27 22:00:00', 3),
+(4, '2018-05-27 22:00:00', 4),
+(5, '2018-05-28 22:00:00', 5),
+(6, '2018-05-28 22:00:00', 6),
+(7, '2018-05-28 22:00:00', 7),
+(8, '2018-05-28 22:00:00', 8),
+(9, '2018-05-29 22:00:00', 9),
+(10, '2018-05-29 22:00:00', 10),
+(11, '2018-05-29 22:00:00', 11),
+(12, '2018-05-29 22:00:00', 12),
+(13, '2018-05-30 22:00:00', 9),
+(14, '2018-05-30 22:00:00', 1),
+(15, '2018-05-30 22:00:00', 7),
+(16, '2018-05-30 22:00:00', 4);
 
 -- --------------------------------------------------------
 
@@ -211,14 +246,20 @@ ALTER TABLE `commande`
 -- Contraintes pour la table `détail_commande`
 --
 ALTER TABLE `détail_commande`
-  ADD CONSTRAINT `fk_Commande_has_Produit_Commande` FOREIGN KEY (`Commande_id`) REFERENCES `commande` (`id`) ON UPDATE CASCADE,
-  ADD CONSTRAINT `fk_Commande_has_Produit_Produit1` FOREIGN KEY (`Produit_id`) REFERENCES `produit` (`id`) ON UPDATE CASCADE;
+  ADD CONSTRAINT `fk_Détail_Commande_Commande1` FOREIGN KEY (`Commande_id`) REFERENCES `commande` (`id`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_Détail_Commande_platsDuJour1` FOREIGN KEY (`produitsDuJour_id`) REFERENCES `produitsdujour` (`id`) ON UPDATE CASCADE;
 
 --
 -- Contraintes pour la table `produit`
 --
 ALTER TABLE `produit`
   ADD CONSTRAINT `fk_Produit_Categorie1` FOREIGN KEY (`Categorie_id`) REFERENCES `categorie` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Contraintes pour la table `produitsdujour`
+--
+ALTER TABLE `produitsdujour`
+  ADD CONSTRAINT `fk_platsDuJour_Produit1` FOREIGN KEY (`Produit_id`) REFERENCES `produit` (`id`) ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
